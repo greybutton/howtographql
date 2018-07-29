d85850a795b3538a8933bffaff2d4fb08233cc96
greybutton <greybutton@yandex.com>
2018-07-29 16:40:08 +0800

    Pagination

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import { FEED_QUERY } from './LinkList'
+import { LINKS_PER_PAGE } from '../constants'
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -40,16 +41,23 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation 
+        <Mutation
           mutation={POST_MUTATION}
-          variables={{ description, url }} 
-          onCompleted={() => this.props.history.push('/')}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push('/new/1')}
           update={(store, { data: { post } }) => {
-            const data = store.readQuery({ query: FEED_QUERY })
+            const first = LINKS_PER_PAGE
+            const skip = 0
+            const orderBy = 'createdAt_DESC'
+            const data = store.readQuery({
+              query: FEED_QUERY,
+              variables: { first, skip, orderBy }
+            })
             data.feed.links.unshift(post)
             store.writeQuery({
               query: FEED_QUERY,
-              data
+              data,
+              variables: { first, skip, orderBy }
             })
           }}
         >
