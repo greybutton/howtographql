5f39b43fdec64f5e1642af5a2a231b4074a518ab
greybutton <greybutton@yandex.com>
2018-08-19 17:38:34 +0800

    Connecting Server and Database with Prisma Bindings

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,54 +1,36 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
-
-// 2
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, args) => links.find(link => link.id === args.id),
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
   },
   Mutation: {
-    post: (root, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
     },
-    updateLink: (root, args) => {
-      const link = links.find(link => link.id === args.id)
-      const index = links.findIndex(link => link.id === args.id)
-      const newLink = {
-        ...link,
-        description: args.description,
-        url: args.url,
-      }
-      const start = links.slice(0, index)
-      const end = links.slice(index + 1)
-      links = [...start, newLink, ...end]
-      return newLink
-    },
-    deleteLink: (root, args) => {
-      const link = links.find(link => link.id === args.id)
-      links = links.filter(link => link.id !== args.id)
-      return link
-    }
-  }
+  },
 }
 
-// 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/greybutton-fe5bae/hackernews-node/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
